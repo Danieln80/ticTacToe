cdf3e9488cd45aa0ffed7af5623bd5af59289548
yosefschmidt <102920177+yosefschmidt@users.noreply.github.com>
2022-05-10 19:53:44 +0300

    Add files via upload

---
+++ clom.js
@@ -0,0 +1,20 @@
+let sumcards=3
+const cardsw=[]
+let div=document.getElementsByClassName("board")
+let creatdiv= document.createElement("div")
+
+for(v = 0; v < sumcards; v++){
+    cardsw[v]=[]
+    for(i = 0; i < sumcards; i++){
+       a=creatdiv.className
+       a.className=i 
+    cardsw[v][i]=a
+    
+    }
+    }
+
+
+ 
+  
+    
+ 
